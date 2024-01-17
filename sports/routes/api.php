041539ee8e2c64c8a3305d3330c4923a8313e659
slavisaj01 <156541517+slavisaj01@users.noreply.github.com>
2024-01-17 20:50:15 +0100

    controller i rute za autorizaciju

--- sports/routes/api.php
+++ sports/routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\User\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
