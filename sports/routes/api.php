ac76246734dedc87ef6744005abbc5b6f050ffed
slavisaj01 <156541517+slavisaj01@users.noreply.github.com>
2024-01-17 21:09:26 +0100

    controler i rute za playera

--- sports/routes/api.php
+++ sports/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\User\AuthController;
+use App\Http\Controllers\PlayerController;
 use App\Http\Controllers\TeamController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,11 @@ Route::get('/teams', [TeamController::class, 'index']);
 Route::get('/teams/paginate', [TeamController::class, 'indexPaginate']);
 Route::get('/teams/{id}', [TeamController::class, 'show']);
 
+Route::get('/players', [PlayerController::class, 'index']);
+Route::get('/players/paginate', [PlayerController::class, 'indexPaginate']);
+Route::get('/players/{id}', [PlayerController::class, 'show']);
+
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -29,6 +35,10 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function (Request $request) {
         return auth()->user();
     });
+    Route::resource('/players', PlayerController::class)
+        ->only(['store', 'update', 'destroy']);
+    Route::resource('/teams', TeamController::class)
+        ->only(['store', 'update', 'destroy']);
 
     
     Route::post('/logout', [AuthController::class, 'logout']);
